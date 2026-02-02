3caba119584d32e3678e476a2d2162cc64e53ad0
Joe Beaudoin <jmcsmith@gmail.com>
2026-02-02 12:49:12 -0500

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MTGJsonClasses",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MTGJsonClasses",
+            targets: ["MTGJsonClasses"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MTGJsonClasses"
+        ),
+        .testTarget(
+            name: "MTGJsonClassesTests",
+            dependencies: ["MTGJsonClasses"]
+        ),
+    ]
+)
