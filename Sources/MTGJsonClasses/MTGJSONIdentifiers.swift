0e7d731c80760e8c019ce4849c67d6d82e7c4745
Joe Beaudoin <jmcsmith@gmail.com>
2026-02-04 09:41:40 -0500

    update IDs

--- Sources/MTGJsonClasses/MTGJSONIdentifiers.swift
+++ Sources/MTGJsonClasses/MTGJSONIdentifiers.swift
@@ -4,16 +4,19 @@ public final class MTGJSONIdentifiers: Codable {
     public var abuId: String?
     public var cardKingdomId: String?
     public var cardKingdomFoilId: String?
+    public var cardKingdomEtchedId: String?
     public var cardsphereId: String?
     public var cardsphereFoilId: String?
     public var cardtraderId: String?
     public var csiId: String?
-    public var deckboxId: String?
     public var mcmId: String?
     public var mcmMetaId: String?
+    public var miniaturemarketId: String?
     public var mtgArenaId: String?
     public var mtgoId: String?
     public var mtgoFoilId: String?
+    public var mtgjsonFoilVersionId: String?
+    public var mtgjsonNonFoilVersionId: String?
     public var mtgjsonV4Id: String?
     public var multiverseId: String?
     public var scgId: String?
@@ -22,5 +25,6 @@ public final class MTGJSONIdentifiers: Codable {
     public var scryfallIllustrationId: String?
     public var scryfallOracleId: String?
     public var tcgplayerProductId: String?
+    public var tcgplayerEtchedProductId: String?
     public var tntId: String?
 }
