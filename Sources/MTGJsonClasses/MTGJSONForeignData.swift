259b5f7ad8aa5a331cd1d1abda093cdcfb463e5d
Joe Beaudoin <jmcsmith@gmail.com>
2026-02-04 09:45:02 -0500

    update to foreign data

--- Sources/MTGJsonClasses/MTGJSONForeignData.swift
+++ Sources/MTGJsonClasses/MTGJSONForeignData.swift
@@ -1,32 +1,33 @@
 import Foundation
 
 public final class MTGJSONForeignData: Codable {
-    public var language: String
-    public var name: String?
-    public var type: String?
-    public var text: String?
+
+    public var faceName: String?
     public var flavorText: String?
-    public var multiverseId: Int?
-    public var uuid: String?
-    public var identifiers: MTGJSONIdentifiers?
+    public var identifiers: MTGJSONIdentifiers
+    public var language: String
+    public var name: String
+    public var text: String?
+    public var type: String?
+    public var uuid: String
 
     public init(
         language: String,
-        name: String? = nil,
-        type: String? = nil,
-        text: String? = nil,
+        name: String,
+        uuid: String,
+        identifiers: MTGJSONIdentifiers,
+        faceName: String? = nil,
         flavorText: String? = nil,
-        multiverseId: Int? = nil,
-        uuid: String? = nil,
-        identifiers: MTGJSONIdentifiers? = nil
+        text: String? = nil,
+        type: String? = nil
     ) {
         self.language = language
         self.name = name
-        self.type = type
-        self.text = text
-        self.flavorText = flavorText
-        self.multiverseId = multiverseId
         self.uuid = uuid
         self.identifiers = identifiers
+        self.faceName = faceName
+        self.flavorText = flavorText
+        self.text = text
+        self.type = type
     }
 }
